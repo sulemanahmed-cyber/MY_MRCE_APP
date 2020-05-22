2eee2c5f2c8c3e086509667921e34020badf3973
Mohammed Suleman <mohdsuleman2626@gmail.com>
2020-05-23 03:09:53 +0530

    spinner tried in student_chap

--- app/src/main/java/com/test/mymrceapp/student_chapters.java
+++ app/src/main/java/com/test/mymrceapp/student_chapters.java
@@ -5,6 +5,12 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -12,10 +18,53 @@ public class student_chapters extends AppCompatActivity {
     Toolbar toolbar;
     NavigationView navigationView;
     DrawerLayout drawerLayout;
+    ArrayAdapter spin_12;
+    Spinner spinner_1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_chapters);
+      /*  new ArrayAdapter<myspin>("hello","hii");*/
+
+        spinner_1 =(findViewById(R.id.spin_1));
+
+
+
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.group1,android.R.layout.simple_spinner_item);
+        spinner_1.setAdapter(adapter);
+
+
+        spinner_1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch(position){
+                    case 0:
+                        Toast.makeText(student_chapters.this, "english_1", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case 1:
+                        Toast.makeText(student_chapters.this, "Hindi_1", Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+            }
+        });
+    /*    spinner_1.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        Toast.makeText(student_chapters.this, "Test_1", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+                }
+
+        );*/
+
+
         navigationView=findViewById(R.id.navigation_menu);
         toolbar=findViewById(R.id.toolbar);
         drawerLayout=findViewById(R.id.drawer_layout);
