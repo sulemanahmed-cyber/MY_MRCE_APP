d291eb4e8fa1428f5d0aabc918f505cf5a063742
Mohammed Suleman <mohdsuleman2626@gmail.com>
2020-05-24 03:05:37 +0530

    bus ui created, student_c new code added.

--- app/src/main/java/com/test/mymrceapp/student_chapters.java
+++ app/src/main/java/com/test/mymrceapp/student_chapters.java
@@ -3,11 +3,15 @@ package com.test.mymrceapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.transition.AutoTransition;
+import androidx.transition.TransitionManager;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.Toast;
@@ -18,37 +22,16 @@ public class student_chapters extends AppCompatActivity {
     Toolbar toolbar;
     NavigationView navigationView;
     DrawerLayout drawerLayout;
-    ArrayAdapter spin_12;
-    Spinner spinner_1;
+    LinearLayout par_csi, expndbl_csi,grp_csi;
+    Button arrowBtn_Csi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_chapters);
-      /*  new ArrayAdapter<myspin>("hello","hii");*/
-
-        spinner_1 =(findViewById(R.id.spin_1));
 
 
 
-        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.group1,android.R.layout.simple_spinner_item);
-        spinner_1.setAdapter(adapter);
-
-
-        spinner_1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                switch(position){
-                    case 0:
-                        Toast.makeText(student_chapters.this, "english_1", Toast.LENGTH_SHORT).show();
-                        break;
-
-                    case 1:
-                        Toast.makeText(student_chapters.this, "Hindi_1", Toast.LENGTH_SHORT).show();
-                        break;
-
-                }
-            }
-        });
     /*    spinner_1.setOnItemSelectedListener(
                 new AdapterView.OnItemSelectedListener() {
                     @Override
@@ -68,6 +51,25 @@ public class student_chapters extends AppCompatActivity {
         navigationView=findViewById(R.id.navigation_menu);
         toolbar=findViewById(R.id.toolbar);
         drawerLayout=findViewById(R.id.drawer_layout);
+        expndbl_csi=findViewById(R.id.expndbl_csi);
+        arrowBtn_Csi=findViewById(R.id.arrowBtn_csi);
+        par_csi=findViewById(R.id.par_csi);
+        grp_csi=findViewById(R.id.grp_csi);
+        grp_csi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (expndbl_csi.getVisibility() == View.GONE) {
+                    TransitionManager.beginDelayedTransition(par_csi, new AutoTransition());
+                    expndbl_csi.setVisibility(View.VISIBLE);
+                    arrowBtn_Csi.setBackgroundResource(R.drawable.ic_keyboard_arrow_up_black_24dp);
+                } else {
+                    TransitionManager.beginDelayedTransition(par_csi, new AutoTransition());
+                    expndbl_csi.setVisibility(View.GONE);
+                    arrowBtn_Csi.setBackgroundResource(R.drawable.ic_keyboard_arrow_down_black_24dp);
+                }
+            }
+        });
+
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
