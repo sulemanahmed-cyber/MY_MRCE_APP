5b260814c69321658ba1447a62ba80e60a1c61ef
satvik246 <64673215+satvik246@users.noreply.github.com>
2020-05-24 21:46:45 +0530

    changed colourd in few activities

--- app/src/main/java/com/test/mymrceapp/canteen.java
+++ app/src/main/java/com/test/mymrceapp/canteen.java
@@ -5,13 +5,24 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
 
 public class canteen extends AppCompatActivity {
     Toolbar toolbar;
     NavigationView navigationView;
     DrawerLayout drawerLayout;
+    AutoCompleteTextView textView;
+    SearchView searchView;
+    ListView listView;
+    ArrayList<String>list1 = new ArrayList<String>();
+    ArrayAdapter<String> arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,10 +30,34 @@ public class canteen extends AppCompatActivity {
         navigationView=findViewById(R.id.navigation_menu);
         toolbar=findViewById(R.id.toolbar);
         drawerLayout=findViewById(R.id.drawer_layout);
+        listView = findViewById(R.id.list);
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        searchView = findViewById(R.id.search);
 
+        list1.add("veg rice         -   60rs");
+        list1.add("veg biryani");
+        list1.add("veg manchurya");
+        list1.add("idli");
+        list1.add("puri");
+        list1.add("chicken rice");
+        list1.add("chicken manchurya");
+        list1.add("chapati");
+        arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,list1);
+        listView.setAdapter(arrayAdapter);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                arrayAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
 
 
     }
