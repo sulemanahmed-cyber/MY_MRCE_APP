c579381247b8ecdab936a09b8e46e2ebbc0774cf
satvik246 <64673215+satvik246@users.noreply.github.com>
2020-06-26 22:37:10 +0530

    Assignment.

--- app/src/main/java/com/test/mymrceapp/assignments.java
+++ app/src/main/java/com/test/mymrceapp/assignments.java
@@ -23,6 +23,9 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
@@ -39,6 +42,8 @@ public class assignments extends AppCompatActivity  {
 
     private static final int PICK_IMAGE_REQUEST = 234;
     ListView listview;
+    EditText editPDFName;
+    DatabaseReference databaseReference;
     Button addButton;
     private ImageView imageView;
     private Button buttonChoose, buttonUpload;
@@ -55,12 +60,15 @@ public class assignments extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assignments);
+        editPDFName = findViewById(R.id.txt_pdfName);
 
         imageView=(ImageView) findViewById(R.id.imageView);
         buttonChoose=(Button) findViewById(R.id.buttonChoose);
         buttonUpload=(Button) findViewById(R.id.buttonUpload);
 
          storageReference=FirebaseStorage.getInstance().getReference();
+        databaseReference = FirebaseDatabase.getInstance().getReference("images");
+
         listview = findViewById(R.id.listView1);
         addButton = findViewById(R.id.button1);
         GetValue = findViewById(R.id.editText1);
@@ -106,13 +114,13 @@ public class assignments extends AppCompatActivity  {
             }
         });
 
-        addButton.setOnClickListener(new View.OnClickListener() {
+       /* addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ListElementsArrayList.add(GetValue.getText().toString());
                 adapter.notifyDataSetChanged();
             }
-        });
+        });*/
         buttonChoose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -129,6 +137,8 @@ public class assignments extends AppCompatActivity  {
     }
 
 
+
+
     private void showFileChooser(){
        Intent intent= new Intent();
        intent.setType("image/*");
@@ -136,6 +146,9 @@ public class assignments extends AppCompatActivity  {
        startActivityForResult(Intent.createChooser(intent,"select an image"), PICK_IMAGE_REQUEST);
 
     }
+
+
+
 
     /*@Override
     public void onClick(View v) {
@@ -150,14 +163,15 @@ public class assignments extends AppCompatActivity  {
     }*/
 
 
-    @Override
+   @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
 
         if (requestCode==PICK_IMAGE_REQUEST && resultCode==RESULT_OK && data != null && data.getData()!=null)
         {
-                filepath=data.getData();
+            uploadPDFFile(data.getData());
+              //  filepath=data.getData();
 
             try {
                 Bitmap bitmap= MediaStore.Images.Media.getBitmap(getContentResolver(),filepath);
@@ -169,6 +183,34 @@ public class assignments extends AppCompatActivity  {
         }
 
     }
+
+    private void uploadPDFFile(final Uri data) {
+        final ProgressDialog progressDialog= new ProgressDialog(this);
+        progressDialog.setTitle("Uploading..");
+        progressDialog.show();
+
+       StorageReference reference = storageReference.child("images/"+System.currentTimeMillis()+".pdf");
+       reference.putFile(data)
+               .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                   @Override
+                   public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                       Task<Uri> uri = taskSnapshot.getStorage().getDownloadUrl();
+                       while (!uri.isComplete());
+                       Uri url = uri.getResult();
+                       uploadPDF uploadPDF = new uploadPDF(editPDFName.getText().toString(),url.toString());
+                       databaseReference.child(databaseReference.push().getKey()).setValue(uploadPDF);
+                       Toast.makeText(assignments.this, "file uploaded", Toast.LENGTH_SHORT).show();
+                       progressDialog.dismiss();
+                   }
+               }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+           @Override
+           public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+               double progress = (100.0*taskSnapshot.getBytesTransferred())/taskSnapshot.getTotalByteCount();
+               progressDialog.setMessage("uploaded"+(int)progress+"%");
+           }
+       });
+    }
+
 
     private void uploadFile(){
 
@@ -207,4 +249,7 @@ public class assignments extends AppCompatActivity  {
         }
     }
 
+    public void btn_action(View view) {
+       startActivity(new Intent(getApplicationContext(),Main2Activity.class));
+    }
 }
